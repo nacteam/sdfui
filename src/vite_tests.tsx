7659a0897fd26498377b87842c29b413fd422ad6
Mark Fomin <mark@difhel.dev>
2024-01-23 16:11:56 +0300

    build: upgraded dependencies, added Vite for easy testing

--- src/vite_tests.tsx
+++ src/vite_tests.tsx
@@ -4,9 +4,12 @@ import './index.scss';
 import App from './App';
 import { ConfigProvider } from './components';
 
+console.log("main.tsx works");
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+console.log(root);
 root.render(
   <React.StrictMode>
     <ConfigProvider>
